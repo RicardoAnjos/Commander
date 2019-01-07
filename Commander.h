b904b5fb45939717219f2383a83c063de7989154
Krzysztof Kapitan <kkapitan@egnyte.com>
2019-01-07 11:17:51 +0100

    Add support for carthage

---
+++ Commander.h
@@ -0,0 +1,19 @@
+//
+//  Commander.h
+//  Commander
+//
+//  Created by Krzysztof on 07/01/2019.
+//  Copyright © 2019 Egnyte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Commander.
+FOUNDATION_EXPORT double CommanderVersionNumber;
+
+//! Project version string for Commander.
+FOUNDATION_EXPORT const unsigned char CommanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commander/PublicHeader.h>
+
+
