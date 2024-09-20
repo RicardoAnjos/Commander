851e577f6c04f7483da17530be0ba989b3d32309
Ricardo Anjos <ranjos@egnyte.com>
2024-09-20 12:42:17 +0100

    Carthage Support

---
+++ Commander.h
@@ -0,0 +1,16 @@
+//
+//  Commander.h
+//  Commander
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Commander.
+FOUNDATION_EXPORT double CommanderVersionNumber;
+
+//! Project version string for Commander.
+FOUNDATION_EXPORT const unsigned char CommanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commander/PublicHeader.h>
+
+
